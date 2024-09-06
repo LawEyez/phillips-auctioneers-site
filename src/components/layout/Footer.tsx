2e05b121ad880617a0b22d534e947032c7691a95
LawEyez <allenojuka10@gmail.com>
2024-09-06 10:54:34 +0300

    Footer cleanup.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,105 @@
 import Brand from '@/components/global/Brand'
+import Link from 'next/link'
+import { PiGithubLogoDuotone } from 'react-icons/pi'
+import { RiFacebookLine, RiInstagramLine, RiLinkedinLine, RiTwitterXLine } from 'react-icons/ri'
+
+import { footerData } from './footerData'
+
+type FooterLinkProps = {
+  name: string
+  url: string
+  external?: boolean
+}
+
+type FooterLinkGroupProps = {
+  title: string
+  links: FooterLinkProps[]
+}
+
+const FooterLink = ({ name, url, external }: FooterLinkProps) => {
+  return (
+    <Link
+      href={url}
+      className='text-[12px] text-neutral-500 hover:text-black hover:underline transition w-max font-primary'
+      target={external ? "_blank" : ""}
+    >
+      {name}
+    </Link>
+  )
+}
+
+const FooterLinkGroup = ({ title, links }: FooterLinkGroupProps) => {
+  return (
+    <div className="space-y-2.5 font-secondary">
+      <h5 className="text-sm font-medium text-black">{title}</h5>
+
+      <div className="flex flex-col space-y-1">
+        {links.map((link, i) => (
+          <FooterLink key={i} name={link.name} url={link.url} external={link.external} />
+        ))}              
+      </div>
+    </div>
+  )
+}
 
 const Footer = () => {
   return (
-    <footer className='bg-neutral-100 py-10'>
-      <div className="container flex flex-col sm:flex-row justify-between gap-10">
-        <div className="">
-          <Brand />
+    <footer className='border-t border-white/10 w-full relative z-10 bg-neutral-100 py-16'>
+      <div className="container space-y-16">
+        {/* ********* TOP ********* */}
+        <div className="flex flex-col gap-10 sm:flex-row sm:justify-between">
+          <div className="flex flex-col gap-3">
+            <Brand />
+            {/* <p className="text-content !text-sm">Building products that matter</p> */}
+            {/* <Modal
+              param="showContact"
+              opener={
+                <button className="btn btn-default w-max">
+                  Work with us
+                </button>
+              }
+            >
+              <ContactForm />
+            </Modal> */}
+          </div>
+
+          <div className="flex gap-10 flex-wrap">
+            <FooterLinkGroup
+              title='Company'
+              links={footerData.company}
+            />
+            
+            <FooterLinkGroup
+              title='Contact'
+              links={footerData.contact}
+            />
+          </div>
+        </div>
+        
+        {/* ********* BOTTOM ********* */}
+        <div className="flex flex-col gap-5 sm:flex-row sm:items-center justify-between relative pt-5
+        border-t border-black/10">
+          <p className="text-[12px] font-medium text-neutral-500">
+            &copy; {new Date().getFullYear()} Phillips Auctioneers. All rights reserved.
+          </p>
+
+          <div className="border border-black/10 rounded-xl py-2 px-3 flex gap-4 w-max">
+            <Link
+              href='https://www.facebook.com/Phillipsauctioneers/'
+              className='text-gray-600 hover:text-black transition'
+              target="_blank"
+            >
+              <RiFacebookLine />
+            </Link>
+            
+            <Link
+              href='https://www.instagram.com/phillipsauctioneers/'
+              className='text-gray-600 hover:text-black transition'
+              target="_blank"
+            >
+              <RiInstagramLine />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
---
+++ src/components/layout/footerData.ts
@@ -0,0 +1,70 @@
+export const footerData = {
+  company: [
+    {
+      name: 'Home',
+      url: '/',
+      external: false,
+    },
+    {
+      name: 'Properties on sale',
+      url: '/#propertiesOnSale',
+      external: false,
+    },
+    // {
+    //   name: 'Careers',
+    //   url: '/',
+    //   external: false,
+    // },
+    // {
+    //   name: 'Blog',
+    //   url: '/',
+    //   external: false,
+    // },
+  ],
+
+  resources: [
+    {
+      name: 'blog',
+      url: '/',
+      external: false,
+    },
+    {
+      name: 'customers',
+      url: '/',
+      external: false,
+    },
+    {
+      name: 'community',
+      url: '/',
+      external: false,
+    },
+    {
+      name: 'events',
+      url: '/',
+      external: false,
+    },
+  ],
+
+  contact: [
+    {
+      name: 'Kandara Road, Kileleshwa',
+      url: 'mailto:info@phillipsauctioneers.co.ke',
+      external: false,
+    },
+    {
+      name: 'info@phillipsauctioneers.co.ke',
+      url: 'mailto:info@phillipsauctioneers.co.ke',
+      external: false,
+    },
+    {
+      name: '+254 (0)721 244 842',
+      url: 'tel:+254-721-244-842',
+      external: false,
+    },
+    {
+      name: '+254 (0)727 872 478',
+      url: 'tel:+254-727-872-478',
+      external: false,
+    },
+  ],
+}
