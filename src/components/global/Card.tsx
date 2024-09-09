eb63bc5e2d66329c5f2e199c7c06b688b5dd106b
LawEyez <allenojuka10@gmail.com>
2024-09-09 12:19:16 +0300

    Add guide price, update styles.

--- src/components/global/Card.tsx
+++ src/components/global/Card.tsx
@@ -15,6 +15,7 @@ type CardProps = {
   location: string
   idx?: number
   slug: string
+  guidePrice: number
 }
 
 const Card = ({
@@ -25,33 +26,43 @@ const Card = ({
   location,
   id,
   idx,
-  slug
+  slug,
+  guidePrice
 }: CardProps) => {
-  const { day, month, year } = formatDate(date)
+  const { day, month, year } = formatDate(new Date(date))
 
   switch(type) {
     case 'sm':
       return (
-        <Link href={`/properties/${slug}?id=${id}`}>
-          <div className='w-[280px] sm:w-[350px]'>
-            <Image
-              src={type === 'sm' ? img as string : ''}
-              width={1920}
-              height={1245}
-              alt=''
-              className='h-40 rounded-lg'
-            />
-
-            <div className="border border-white/10 bg-white/5 rounded-lg p-5 space-y-5 backdrop-blur-2xl
-            mt-[-20px]">
-              <div className="text-primary">
+        <Link href={`/properties/${id}`}>
+          <div className="w-[400px] border border-white/10 bg-gradient-to-b from-white/5 to-transparent rounded-xl p-5
+          space-y-5 backdrop-blur-2xl group">
+            <div className="flex justify-between">
+              <div className="text-primary p-3 border border-white/10 rounded-xl w-max">
                 <p className="text-2xl font-bold">{day}</p>
                 <p className="text-xs uppercase">{month}</p>
               </div>
 
-              <p className="text-sm">
-                {title}
-              </p>
+              <Button className='border border-primary/10 rounded-full text-sm font-medium w-max
+              h-max flex items-center gap-2 py-2 px-3 group-hover:bg-black group-hover:text-white transition'>
+                View property
+                <span className="h-5 w-5 bg-black text-white rounded-full flex items-center justify-center text-lg">
+                  <RiArrowRightUpLine />
+                </span>
+              </Button>
+            </div>
+
+            <p className="text-sm text-neutral-200 leading-relaxed">
+              {title}
+            </p>
+
+            <div className="border-t border-white/10 pt-5 flex justify-between">
+              <div className="">
+                <p className="text-sm text-neutral-300">Guide Price (KES)</p>
+                <p className="">{guidePrice.toLocaleString()}</p>
+              </div>
+
+              
             </div>
           </div>
         </Link>
@@ -59,20 +70,29 @@ const Card = ({
 
     case 'lg':
       return (
-        <Link href={`/properties/${slug}?id=${id}`}>
+        <Link href={`/properties/${id}`}>
           <div className='border border-black/10 rounded-xl p-5 flex gap-5 bg-white group hover:border-black transition'>
-            <div className="flex items-center justify-center border-r border-black/10 pr-5">
+            <div className="flex items-center justify-center border-r border-black/10 pr-5 h-max">
               <span className='text-primary/30 text-4xl group-hover:text-black transition'>{(idx ?? 0) + 1}</span>
             </div>
 
-            <div className="space-y-3 flex-1">
+            <div className="space-y-4 flex-1">
               <p className="text-sm md:text-base text-neutral-800 w-full">
                 {title}
               </p>
 
-              <div className="flex items-center gap-4">
-                <p className="text-sm text-neutral-500 font-medium">{day} {month} {year}</p>
-                <div className="px-2 py-1 border border-black/10 rounded-full text-sm bg-neutral-50">{location}</div>
+              <div className="flex gap-5">
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm text-neutral-500">Guide Price (KES)</p>
+                  <p className="text-sm md:text-base font-medium">{guidePrice.toLocaleString()}</p>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm text-neutral-500">Auction Date</p>
+                  <p className="text-sm md:text-base font-medium">{day} {month} {year}</p>
+                </div>
+
+                <div className="px-2 py-1 border border-black/10 rounded-full text-sm bg-neutral-50 h-max">{location}</div>
               </div>
             </div>
 
