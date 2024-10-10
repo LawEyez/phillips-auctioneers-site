885fafe1fd3a76c4e8edbdd83b9a0d2ff38f4d6a
LawEyez <allenojuka10@gmail.com>
2024-10-10 10:52:17 +0300

    Make header dark.

--- src/app/properties/[id]/page.tsx
+++ src/app/properties/[id]/page.tsx
@@ -37,32 +37,34 @@ export default async function Detail({ params, searchParams }: { params: { id: s
   const property = properties.find(prop => prop.id === parseInt(id))
   
   return (
-    <section className="py-16 container min-h-[calc(100vh-462px)]">
-      <div className="flex flex-col gap-8">
-        <div className="space-y-5">
-          <h1 className="text-xl sm:text-2xl font-semibold font-secondary">
-            {property?.title}
-          </h1>
+    <section className="min-h-[calc(100vh-462px)] pb-16">
+      <div className="">
+        <div className="py-16 bg-gradient-to-b from-neutral-800 to-neutral-950">
+          <div className="container space-y-8">
+            <h1 className="text-xl sm:text-2xl font-semibold font-secondary text-white">
+              {property?.title}
+            </h1>
 
-          <div className="flex gap-6">
-            <div className="flex flex-col gap-1">
-              <p className="text-sm text-neutral-500">Guide Price (KES)</p>
-              <p className="text-sm md:text-base font-medium">{property?.guidePrice.toLocaleString()}</p>
+            <div className="flex gap-8">
+              <div className="flex flex-col gap-1">
+                <p className="text-sm text-neutral-300">Auction Date</p>
+                <p className="text-sm md:text-base font-medium text-white">{property?.auctionDate}</p>
+              </div>
+
+              <div className="flex flex-col gap-1">
+                <p className="text-sm text-neutral-300">Guide Price (KES)</p>
+                <p className="text-sm md:text-base font-medium text-white">{property?.guidePrice.toLocaleString()}</p>
+              </div>
+
+              <div className="px-3 py-1 border border-white/10 rounded-full text-sm bg-neutral-800 h-max text-sky-200">
+                {property?.location}
+              </div>
             </div>
-
-            <div className="flex flex-col gap-1">
-              <p className="text-sm text-neutral-500">Auction Date</p>
-              <p className="text-sm md:text-base font-medium">{property?.auctionDate}</p>
-            </div>
-
-            <div className="px-2 py-1 border border-black/10 rounded-full text-sm bg-neutral-50 h-max">{property?.location}</div>
           </div>
         </div>
 
-        <LineSeparator type="dark" />
-
         <div
-          className="prose prose-sm md:prose-base max-w-none"
+          className="prose prose-sm md:prose-base max-w-none container py-10"
           dangerouslySetInnerHTML={{ __html: property?.content as string}}
         />
       </div>
